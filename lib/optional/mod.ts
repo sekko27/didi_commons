83881592bf170e37d09f7387222091c0301a01c6
Balazs <sekko272727@gmail.com>
2022-03-03 11:24:29 +0100

    Export types.

--- lib/optional/mod.ts
+++ lib/optional/mod.ts
@@ -1,3 +1,9 @@
 export type { IEmpty } from "./empty/IEmpty.ts";
 export { Empty } from "./empty/Empty.ts";
 export { Optional } from "./Optional.ts";
+export type { IAsyncOptional } from "./async/interfaces/IAsyncOptional.ts";
+export type { IAsyncOptionalValueProvider } from "./async/interfaces/IAsyncOptionalValueProvider.ts";
+export type { ILazySyncOptional } from "./sync/interfaces/ILazySyncOptional.ts";
+export type { ISyncOptional } from "./sync/interfaces/ISyncOptional.ts";
+export type { IOptionalFactory } from "./sync/interfaces/IOptionalFactory.ts";
+export type { IOptionalValueProvider } from "./sync/interfaces/IOptionalValueProvider.ts";
